c096121fd134e44467eb75b7e65fb7c35a163e37
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2019-01-23 21:19:28 -0200

    creating a Header.js

---
+++ src/components/header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="flex pa1 justify-between nowrap orange">
+                <div className="flex flex-fixed black">
+                    <div className="fw7 mr1">
+                        Movies I've Watched
+                    </div>
+                    <Link to="/" className="ml1 no-underline black">
+                        new
+                    </Link>
+                    <div className="ml1">|</div>
+                    <Link to="/create" className="ml1 no-underline black">
+                        Submit
+                    </Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default withRouter(Header);
