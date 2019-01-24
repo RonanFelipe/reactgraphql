6c206c782ccb78061abd0450664104e5ceadea33
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2019-01-23 22:57:04 -0200

    add route for actor component

--- src/components/header.js
+++ src/components/header.js
@@ -11,11 +11,17 @@ class Header extends Component {
                         Movies I've Watched
                     </div>
                     <Link to="/" className="ml1 no-underline black">
-                        new
+                        List Movies
                     </Link>
                     <div className="ml1">|</div>
                     <Link to="/create" className="ml1 no-underline black">
-                        Submit
+                        Create Movies
+                    </Link>
+                    <Link to="/listActors" className="ml1 no-underline black">
+                        List Actors
+                    </Link>
+                    <Link to="/createActor" className="ml1 no-underline black">
+                        Create Actors
                     </Link>
                 </div>
             </div>
