9c999511d230c7a1affbef598c04ab4e6de2d343
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2019-01-25 23:45:13 -0200

    add login link on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
+import {AUTH_TOKEN} from "../constants";
 
 class Header extends Component {
     render() {
+        const authToken = localStorage.getItem(AUTH_TOKEN);
         return (
             <div className="flex pa1 justify-between nowrap orange">
                 <div className="flex flex-fixed black">
@@ -14,17 +16,38 @@ class Header extends Component {
                         List Movies
                     </Link>
                     <div className="ml1">|</div>
-                    <Link to="/create" className="ml1 no-underline black">
-                        Create Movies
-                    </Link>
-                    <div className="ml1">|</div>
                     <Link to="/listActors/" className="ml1 no-underline black">
                         List Actors
                     </Link>
-                    <div className="ml1">|</div>
-                    <Link to="/createActor" className="ml1 no-underline black">
-                        Create Actors
-                    </Link>
+                    {authToken && (
+                        <div className="flex">
+                            <div className="ml1">|</div>
+                            <Link to="/create" className="ml1 no-underline black">
+                                Create Movies
+                            </Link>
+                            <div className="ml1">|</div>
+                            <Link to="/createActor" className="ml1 no-underline black">
+                                Create Actors
+                            </Link>
+                        </div>
+                    )}
+                </div>
+                <div className="flex flex-fixed">
+                    {authToken ? (
+                        <div
+                            className="ml1 pointer black"
+                            onClick={() => {
+                                localStorage.removeItem(AUTH_TOKEN)
+                                this.props.history.push(`/`)
+                            }}
+                        >
+                            logout
+                        </div>
+                    ) : (
+                        <Link to="/login" className="ml1 no-underline black">
+                            Login
+                        </Link>
+                    )}
                 </div>
             </div>
         )
